015f3d824c5d196961bd1229ec437f322bddc125
Jigsawfoley <desfoley@hotmail.com>
2018-04-06 21:53:10 +0100

    new branch setup dev-v11

--- run.bat
+++ run.bat
@@ -1,7 +1,15 @@
-@ECHO	OFF
+@ECHO off
 
-"C:\Program Files (x86)\Java\jdk1.8.0_162\bin\java.exe"  
+"C:\Program Files (x86)\Java\jdk1.8.0_162\bin\java.exe" 
 
--cp target/MusicProject-V1.jar FoleyDes.MusicProject.App -d jdbc:sqlite:database/NewFoleyDes.db
+java -cp target\MusicProject-V1.jar FoleyDes.MusicProject.App -d jdbc:sqlite:database/NewFoleyDes.db
+
+java -cp  target\MusicProject-V1.jar FoleyDes.MusicProject.App -v
+
+java -cp  target\MusicProject-V1.jar FoleyDes.MusicProject.App -version
+
+java -cp  target\MusicProject-V1.jar FoleyDes.MusicProject.App -h
+
+java -cp  target\MusicProject-V1.jar FoleyDes.MusicProject.App 
 
 PAUSE
--- src/main/java/FoleyDes/MusicProject/data/IDataManager.java
+++ src/main/java/FoleyDes/MusicProject/data/IDataManager.java
@@ -1,7 +1,12 @@
 package	FoleyDes.MusicProject.data;
+
 import	java.sql.Connection;
 import	java.sql.SQLException;
+
 public interface IDataManager {
+	
 public	Connection	getConnectionObject() throws	SQLException;
+
 public void disconnect() throws	SQLException;
+
 }
